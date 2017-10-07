0de086acbba4bf2208e0ab4837a74c702d4d3372
Saurabh Nema <sauravnemabhopal@gmail.com>
2017-10-07 11:01:36 +0530

    Update audio_file.py

--- audio_file.py
+++ audio_file.py
@@ -5,10 +5,11 @@ import numpy as np
 import scipy.io.wavfile as wav
 
 fs=44100 #Best audio frequency
-duration = 10  # seconds
-myrecording = sd.rec(duration *fs , samplerate=fs, channels=2, dtype='float64')
+duration = 10  # this is a time slot
+myrecording = sd.rec(duration *fs , samplerate=fs, channels=2, dtype='float64') # this are the values that are given to the recorder which is present in the sounddevice file.
 print("Recording Audio")
-sd.wait()
-sd.play(myrecording,fs)
-sd.wait()
+sd.wait() # wait to sync
+sd.play(myrecording,fs) 
+# play your recorded audio
+sd.wait()  # again wait to sync
 print("Play Audio Complete")
